52e7833d0f696018f0478e8cb2f2cc7ece0cb114
wfy <2213305799@qq.com>
2022-11-09 18:53:19 +0800

    init

---
+++ cmake-build-debug/CMakeFiles/CPP_prime_note.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CPP_prime_note.dir/main.cpp.obj"
+  "CPP_prime_note.exe"
+  "CPP_prime_note.exe.manifest"
+  "CPP_prime_note.pdb"
+  "libCPP_prime_note.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CPP_prime_note.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
